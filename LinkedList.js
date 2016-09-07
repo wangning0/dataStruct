31b5c8eaf5a75b642e1f055589f4486ef62f2548
wangning0 <172234437@qq.com>
2016-09-07 08:22:15 +0800

    LList

--- LinkedList.js
+++ LinkedList.js
@@ -6,16 +6,17 @@
   * * LinkedList类提供插入节点、删除节点、显示列表元素的方法，以及其他一些辅助方法
   * Node类
   * * 属性
-  * * * element 用来保存节点上的数据
-  * * * next    用来保存指向下一个节点的链接
+  * * * element       用来保存节点上的数据
+  * * * next          用来保存指向下一个节点的链接
   * LinkedList类
   * * 属性
-  * * * head    头节点
-  * * 方法
-  * * * find    遍历链表
-  * * * insert  插入新节点
-  * * * remove  从链表中删除一个节点
-  * * * display 输出链表内的元素
+  * * * head          头节点
+  * * 方法  
+  * * * find          遍历链表
+  * * * insert        插入新节点
+  * * * remove        从链表中删除一个节点
+  * * * display       展示链表内的元素
+  * * * findPrevious  查找前一个节点(主要用于remove操作时)
 */
 
 //Node类
@@ -31,13 +32,71 @@ function LList() {
   this.insert = insert;
   this.remove = remove;
   this.display = display;
+  this.findPrevious = findPrevious;
 }
 
 //遍历链表
 function find(item) {
   var currNode = this.head;
-  while(currNode.element != item) {
+  console.log(currNode);
+  //进行与操作的顺序不能改变
+  while(currNode != null && currNode.element != item) {
+    console.log(1);
     currNode = currNode.next;
   }
   return currNode;
 }
+//插入新节点
+function insert(newElement, item) {
+  var newNode = new Node(newElement);
+  var current = this.find(item);
+  //判断是否存在插入的节点
+  if(current) {
+    newNode.next = current.next;
+    current.next = newNode;
+  } else {
+    console.log('该节点不存在\n');
+  }
+}
+//展示链表内的所有元素
+function display() {
+  var currNode = this.head;
+  while(currNode.next != null) {
+    console.log(currNode.next.element);
+    currNode = currNode.next;
+  }
+}
+//查找前一个节点
+function findPrevious(item) {
+  var currNode = this.head;
+  while(currNode.next != null && currNode.next.element != item) {
+    currNode = currNode.next;
+  }
+  //判断查找的节点是否存在
+  if(!currNode.next) { 
+    return null;
+  } else {
+    return currNode;
+  }
+}
+//从链表中删除一个节点
+function remove(item) {
+  var prevNode = this.findPrevious(item);
+  if(item) {
+    prevNode.next = prevNode.next.next;
+  } else {
+    console.log('该节点不存在');
+  }
+}
+//test
+var s = new LList();
+s.insert('CaiShiRan','head');
+s.insert('CaiLaoShi','CaiShiRan');
+s.insert('XiaoGe','CaiLaoShi');
+s.insert('ZhangQiLing','XiaoGe');
+s.display();
+
+console.log('-------------------分隔符------------------------');
+
+s.remove('XiaoGe');
+s.display();
